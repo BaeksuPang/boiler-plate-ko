3868311e39e2f90c98fa02dae871117b06f00733
BaeksuPang <zmfltmxl11@gmail.com>
2021-06-22 00:39:04 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI:'mongodb+srv://pangpang:dkfhsk123@boilerplate.so6t3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://pangpang:dkfhsk123@boilerplate.so6t3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('안녕하세요')
+  res.send('안녕하세요 파볼오브소서')
+})
+
+app.post('/register',(req, res) => {
+  //회원 가입 시 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
